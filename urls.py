16c8cc029edf5f44ed6bb4dbb861dee92e79ceae
Anders Pearson <anders@columbia.edu>
2010-08-05 15:46:48 -0400

    set up skeletal pagetree site

--- urls.py
+++ urls.py
@@ -3,18 +3,22 @@ from django.contrib import admin
 from django.conf import settings
 import os.path
 admin.autodiscover()
-import staticmedia
+import djangowind.urls
+import pagetree.urls
 
 site_media_root = os.path.join(os.path.dirname(__file__),"media")
 
 urlpatterns = patterns('',
-                       # Example:
-                       # (r'^diabeaters/', include('diabeaters.foo.urls')),
+                       (r'^$','diabeaters.main.views.index'),
+                       (r'^home/$','diabeaters.main.views.home'),
                        ('^accounts/',include('djangowind.urls')),
                        (r'^admin/(.*)', admin.site.root),
-		       (r'^survey/',include('survey.urls')),
-                       (r'^tinymce/', include('tinymce.urls')),
+                       (r'^pagetree/',include('pagetree.urls')),
                        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media_root}),
                        (r'^uploads/(?P<path>.*)$','django.views.static.serve',{'document_root' : settings.MEDIA_ROOT}),
-) + staticmedia.serve()
+                       # very important that these two stay last and in this order
+                       (r'^edit/(?P<path>.*)$','diabeaters.main.views.edit_page'),
+                       (r'^(?P<path>.*)$','diabeaters.main.views.page'),
+
+) 
 
