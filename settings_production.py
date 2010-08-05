467788f8690f8a9ab8d8d93b930b24dcf7610132
Anders Pearson <anders@columbia.edu>
2010-08-05 14:52:43 -0400

    initializing

---
+++ settings_production.py
@@ -0,0 +1,15 @@
+from settings_shared import *
+
+TEMPLATE_DIRS = (
+    "/var/www/diabeaters/diabeaters/templates",
+)
+
+MEDIA_ROOT = '/var/www/diabeaters/uploads/'
+# put any static media here to override app served static media
+STATICMEDIA_MOUNTS = (
+    ('/sitemedia', '/var/www/diabeaters/diabeaters/sitemedia'),	
+)
+
+
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
