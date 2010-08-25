c9f5766ac51fd1bcb036fa31344f5744cd24c80d
Anders Pearson <anders@columbia.edu>
2010-08-25 11:52:28 -0400

    bring in templates

--- healthhabitplan/views.py
+++ healthhabitplan/views.py
@@ -6,8 +6,24 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response
 from models import *
 
+class rendered_with(object):
+    def __init__(self, template_name):
+        self.template_name = template_name
+
+    def __call__(self, func):
+        def rendered_func(request, *args, **kwargs):
+            items = func(request, *args, **kwargs)
+            if type(items) == type({}):
+                return render_to_response(self.template_name, items, context_instance=RequestContext(request))
+            else:
+                return items
+
+        return rendered_func
+
+@rendered_with('healthhabitplan/index.html')
 def index(request):
     user = request.user
     if request.user.is_anonymous():
         return HttpResponseRedirect("/accounts/login/")
-    return HttpResponse("this is the health habit plan")
+    
+    return dict(user=user)
