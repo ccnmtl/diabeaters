d59830877a06699146d800e789de2690b5e13349
Anders Pearson <anders@columbia.edu>
2010-08-25 14:13:53 -0400

    put items inline in admin form

--- healthhabitplan/admin.py
+++ healthhabitplan/admin.py
@@ -2,8 +2,14 @@ from models import Category, Item, Session, Magnet
 
 from django.contrib import admin
 
-admin.site.register(Category)
-admin.site.register(Item)
+class ItemInline(admin.StackedInline):
+    model = Item
+    extra = 2
+
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [ItemInline,]
+
+admin.site.register(Category,CategoryAdmin)
 admin.site.register(Session)
 admin.site.register(Magnet)
 
