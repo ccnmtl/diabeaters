ccff5b142455801b4ae8bbc59f7e1324d1d9d7a9
Anders Pearson <anders@columbia.edu>
2010-08-25 14:09:24 -0400

    super simple admin

---
+++ healthhabitplan/admin.py
@@ -0,0 +1,9 @@
+from models import Category, Item, Session, Magnet
+
+from django.contrib import admin
+
+admin.site.register(Category)
+admin.site.register(Item)
+admin.site.register(Session)
+admin.site.register(Magnet)
+
