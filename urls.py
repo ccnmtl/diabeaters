b5f665f1b845fbfcd791f5a2c3921166ecd6a4c7
Anders Pearson <anders@columbia.edu>
2010-10-08 14:31:07 -0400

    using central Sentry logging now

--- urls.py
+++ urls.py
@@ -18,7 +18,6 @@ urlpatterns = patterns('',
                        (r'^health-habit-plan/',include('healthhabitplan.urls')),
                        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media_root}),
                        (r'^uploads/(?P<path>.*)$','django.views.static.serve',{'document_root' : settings.MEDIA_ROOT}),
-                       (r'^sentry/', include('sentry.urls')),
                        # very important that these two stay last and in this order
                        (r'^edit/(?P<path>.*)$','diabeaters.main.views.edit_page'),
                        (r'^instructor/(?P<path>.*)$','diabeaters.main.views.instructor_page'),
