7eee39d5f1b5699754f47b36029177d52876b6bc
khagan <khagan@columbia.edu>
2010-10-11 10:51:57 -0400

    popups/trash (with positioning bugs)

--- healthhabitplan/urls.py
+++ healthhabitplan/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('healthhabitplan.views',
                        (r'^del_session/(?P<id>\d+)/$','del_session',{},'health-habit-plan-del-session'),
                        (r'^session/(?P<id>\d+)/$','session',{},'health-habit-plan-session'),
                        (r'^session/(?P<id>\d+)/save_magnet/$','save_magnet',{},'health-habit-plan-save-magnet'),
+                       (r'^session/(?P<id>\d+)/delete_magnet/$','delete_magnet',{},'health-habit-plan-delete-magnet'),
 )
