f47ae3f712cb5f240ff4eadffd3844afeefaecda
Anders Pearson <anders@columbia.edu>
2012-11-27 12:04:10 -0500

    convert to new style database config

--- settings_production.py
+++ settings_production.py
@@ -14,6 +14,18 @@ STATICMEDIA_MOUNTS = (
 DEBUG = False
 TEMPLATE_DEBUG = True
 SENTRY_SITE = "diabeaters"
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'diabeaters',
+        'HOST' : '',
+        'PORT' : 5432,
+        'USER' : '',
+        'PASSWORD' : '',
+        }
+}
+
 try:
     from local_settings import *
 except ImportError:
