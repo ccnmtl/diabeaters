340c347dfd44c7c82dc2dbe4944944358392870a
Anders Pearson <anders@columbia.edu>
2012-12-03 15:30:15 -0500

    remove unused old version

--- settings_shared.py
+++ settings_shared.py
@@ -51,10 +51,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'diabeaters.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    # Put application templates before these fallback ones:
     "/var/www/diabeaters/templates/",
     os.path.join(os.path.dirname(__file__),"templates"),
 )
