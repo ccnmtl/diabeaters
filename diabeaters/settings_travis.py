93be3bc54ec41f542d1b7737180b4801610eac92
Anders Pearson <anders@columbia.edu>
2013-01-11 15:55:14 -0500

    set up a settings file specifically for travis-ci + postgres

---
+++ diabeaters/settings_travis.py
@@ -0,0 +1,17 @@
+from settings_shared import *
+
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'diabeaters',
+        'HOST' : '127.0.0.1',
+        'PORT' : 5432,
+        'USER' : 'postgres',
+        'PASSWORD' : '',
+        }
+}
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
