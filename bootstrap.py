a615e43be7c707d50d569562bea58c28c30d04b0
Anders Pearson <anders@columbia.edu>
2013-07-08 11:29:29 -0400

    pypi mirror

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import glob
 import os
 import sys
 import subprocess
@@ -20,17 +19,18 @@ ret = subprocess.call(["python", "virtualenv.py",
 if ret: exit(ret)
 
 ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
-                       "-E", vedir,
-                       "--index-url=''",
-                       "--requirement",os.path.join(pwd,"requirements/apps.txt")])
+                       "--index-url=http://pypi.ccnmtl.columbia.edu/",
+                       "--requirement",os.path.join(pwd,"requirements.txt")])
 if ret: exit(ret)
 
 if sys.version_info < (2, 7, 0):
     ret = subprocess.call([os.path.join(vedir, 'bin', 'pip'), "install",
                            "-E", vedir,
-                           os.path.join(pwd,"requirements/src/importlib-1.0.1.tar.gz")])
+                           "importlib==1.0.1"])
 
 
 ret = subprocess.call(["python","virtualenv.py","--relocatable",vedir])
 # --relocatable always complains about activate.csh, which we don't really
 # care about. but it means we need to ignore its error messages
+
+
