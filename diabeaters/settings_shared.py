3d6c0faa7e35355d6ef431b6da34e3838c717fe1
Anders Pearson <anders@columbia.edu>
2015-09-25 16:07:31 +0200

    ccnmtlsettings

--- diabeaters/settings_shared.py
+++ diabeaters/settings_shared.py
@@ -1,49 +1,15 @@
 # Django settings for diabeaters project.
 import os.path
-import sys
+from ccnmtlsettings.shared import common
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+project = 'diabeaters'
+base = os.path.dirname(__file__)
 
-ADMINS = ()
-
-MANAGERS = ADMINS
-
-ALLOWED_HOSTS = [
-    ".ccnmtl.columbia.edu",
-    "localhost"]
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'diabeaters',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUEST': True,
-    }
-}
-
-if 'test' in sys.argv or 'jenkins' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-            'ATOMIC_REQUEST': True,
-        }
-    }
-
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
-
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-)
+locals().update(
+    common(
+        project=project,
+        base=base,
+    ))
 
 PROJECT_APPS = [
     'diabeaters.main',
@@ -51,88 +17,14 @@ PROJECT_APPS = [
     'diabeaters.healthhabitplan',
 ]
 
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/diabeaters/uploads/"
-MEDIA_URL = '/uploads/'
-ADMIN_MEDIA_PREFIX = '/media/'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.template.context_processors.debug',
-    'django.template.context_processors.request',
-    'django.template.context_processors.static',
-    'stagingcontext.staging_processor',
-    'djangowind.context.context_processor',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'waffle.middleware.WaffleMiddleware',
-    'impersonate.middleware.ImpersonateMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
-ROOT_URLCONF = 'diabeaters.urls'
-
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS += [  # noqa
     'sorl.thumbnail',
-    'django.contrib.admin',
     'pagetree',
     'pageblocks',
     'diabeaters.quiz',
     'diabeaters.main',
     'diabeaters.healthhabitplan',
-    'django_statsd',
-    'smoketest',
-    'debug_toolbar',
-    'django_jenkins',
-    'waffle',
-    'impersonate',
-    'django_markwhat',
-    'gunicorn',
-    'compressor',
 ]
-
-INTERNAL_IPS = ('127.0.0.1', )
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-)
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'diabeaters'
-STATSD_HOST = 'localhost'
-STATSD_PORT = 8125
-if 'test' in sys.argv:
-    STATSD_HOST = '127.0.0.1'
-
-SENTRY_SITE = "diabeaters-dev"
 
 PAGEBLOCKS = ['pageblocks.TextBlock',
               'quiz.Quiz',
@@ -142,46 +34,5 @@ PAGEBLOCKS = ['pageblocks.TextBlock',
               'pageblocks.ImagePullQuoteBlock']
 
 THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[diabeaters] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "diabeaters@ccnmtl.columbia.edu"
-
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', 'sitemedia'),
-)
-
-STATIC_URL = "/media/"
-STATICFILES_DIRS = ("media/",)
-STATIC_ROOT = "/tmp/diabeaters/static"
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = STATIC_ROOT
-
-
-# WIND settings
-
-AUTHENTICATION_BACKENDS = ('djangowind.auth.SAMLAuthBackend',
-                           'django.contrib.auth.backends.ModelBackend', )
-
-CAS_BASE = "https://cas.columbia.edu/"
-WIND_PROFILE_HANDLERS = ['djangowind.auth.CDAPProfileHandler']
-WIND_AFFIL_HANDLERS = ['djangowind.auth.AffilGroupMapper',
-                       'djangowind.auth.StaffMapper',
-                       'djangowind.auth.SuperuserMapper']
-WIND_STAFF_MAPPER_GROUPS = ['tlc.cunix.local:columbia.edu']
-WIND_SUPERUSER_MAPPER_GROUPS = ['anp8', 'jb2410', 'zm4', 'egr2107',
-                                'kmh2124', 'sld2131', 'amm8', 'mar227',
-                                'mj2402', 'mar227']
 
 AUTH_PROFILE_MODULE = "main.UserProfile"
-LOGIN_REDIRECT_URL = "/"
-
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
-SECRET_KEY = "asdfwertsdbsdrtfy"
--- diabeaters/settings_staging.py
+++ diabeaters/settings_staging.py
@@ -1,56 +1,14 @@
 # flake8: noqa
 from settings_shared import *
-import os
-import sys
+from ccnmtlsettings.staging import common
 
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-MEDIA_ROOT = '/var/www/diabeaters/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/diabeaters/diabeaters/sitemedia'),
-)
-
-
-DEBUG = False
-TEMPLATE_DEBUG = True
-STAGING_ENV = True
-
-
-DATABASES = {
-    'default' : {
-        'ENGINE' : 'django.db.backends.postgresql_psycopg2',
-        'NAME' : 'diabeaters',
-        'HOST' : '',
-        'PORT' : 6432,
-        'USER' : '',
-        'PASSWORD' : '',
-        'ATOMIC_REQUEST': True,
-        }
-}
-
-AWS_STORAGE_BUCKET_NAME = "ccnmtl-diabeaters-static-stage"
-AWS_PRELOAD_METADATA = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'diabeaters.s3utils.CompressorS3BotoStorage'
-S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-COMPRESS_ROOT = STATIC_ROOT
-COMPRESS_URL = STATIC_URL
-DEFAULT_FILE_STORAGE = 'diabeaters.s3utils.MediaRootS3BotoStorage'
-MEDIA_URL = S3_URL + '/media/'
-COMPRESS_STORAGE = 'diabeaters.s3utils.CompressorS3BotoStorage'
-AWS_QUERYSTRING_AUTH = False
-
-
-STATSD_PREFIX = 'diabeaters-staging'
-
-if 'migrate' not in sys.argv:
-    INSTALLED_APPS.append('raven.contrib.django.raven_compat')
+locals().update(
+    common(
+        project=project,
+        base=base,
+        INSTALLED_APPS=INSTALLED_APPS,
+        STATIC_ROOT=STATIC_ROOT,
+    ))
 
 try:
     from local_settings import *
