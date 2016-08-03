bffccc7df8a92785f6389365d3a28f37fd2e6967
Anders Pearson <anders@columbia.edu>
2016-08-03 14:11:09 +0100

    Django 1.10 urls
    convert to new style urls.py

--- diabeaters/urls.py
+++ diabeaters/urls.py
@@ -1,34 +1,39 @@
-from django.conf.urls import patterns, include, url
+import django.contrib.auth.views
+import django.views.static
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.views.generic import TemplateView
+from diabeaters.main.views import (
+    index, export, import_, flatpage_hack, home, edit_page,
+    instructor_page, page,
+)
 admin.autodiscover()
 
 
-urlpatterns = patterns(
-    '',
-    (r'^$', 'diabeaters.main.views.index'),
+urlpatterns = [
+    url(r'^$', index),
     url(r'^_impersonate/', include('impersonate.urls')),
-    (r'^export/$', 'diabeaters.main.views.export'),
-    (r'^import/$', 'diabeaters.main.views.import_'),
-    (r'^about/$', 'diabeaters.main.views.flatpage_hack'),
-    (r'^credits/$', 'diabeaters.main.views.flatpage_hack'),
-    (r'^contact/$', 'diabeaters.main.views.flatpage_hack'),
+    url(r'^export/$', export),
+    url(r'^import/$', import_),
+    url(r'^about/$', flatpage_hack),
+    url(r'^credits/$', flatpage_hack),
+    url(r'^contact/$', flatpage_hack),
 
-    (r'^home/$', 'diabeaters.main.views.home'),
-    (r'^logout/$', 'django.contrib.auth.views.logout',
-     {'next_page': '/home/'}),
-    ('^accounts/', include('djangowind.urls')),
-    (r'^smoketest/', include('smoketest.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^pagetree/', include('pagetree.urls')),
-    (r'^quiz/', include('diabeaters.quiz.urls')),
-    (r'^health-habit-plan/', include('diabeaters.healthhabitplan.urls')),
-    (r'^uploads/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-    (r'^_stats/', TemplateView.as_view(template_name="main/stats.html")),
+    url(r'^home/$', home),
+    url(r'^logout/$', django.contrib.auth.views.logout,
+        {'next_page': '/home/'}),
+    url('^accounts/', include('djangowind.urls')),
+    url(r'^smoketest/', include('smoketest.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^pagetree/', include('pagetree.urls')),
+    url(r'^quiz/', include('diabeaters.quiz.urls')),
+    url(r'^health-habit-plan/', include('diabeaters.healthhabitplan.urls')),
+    url(r'^uploads/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^_stats/', TemplateView.as_view(template_name="main/stats.html")),
     # very important that these three stay last and in this order
-    (r'^edit/(?P<path>.*)$', 'diabeaters.main.views.edit_page'),
-    (r'^instructor/(?P<path>.*)$', 'diabeaters.main.views.instructor_page'),
-    (r'^(?P<path>.*)$', 'diabeaters.main.views.page'),
-)
+    url(r'^edit/(?P<path>.*)$', edit_page),
+    url(r'^instructor/(?P<path>.*)$', instructor_page),
+    url(r'^(?P<path>.*)$', page),
+]
